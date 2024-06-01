2e83c3f56508203873f2816aabef1ed4739c6010
_SongJ <songj9507@outlook.com>
2024-06-01 10:22:00 +0800

    add mnocoAp

--- sosmetrics/metrics/__init__.py
+++ sosmetrics/metrics/__init__.py
@@ -2,6 +2,7 @@ from .auc_roc_pr_metric import AUC_ROC_PRMetric
 from .base import BaseMetric, time_cost_deco
 from .binary_center_metric import (BinaryCenterAveragePrecisionMetric,
                                    BinaryCenterMetric)
+from .mNoCoAP import mNoCoAP
 from .pd_fa_metric import PD_FAMetric
 from .pre_rec_f1_iou_metric import Precision_Recall_F1_IoUMetric
 from .utils import channels2last, convert2batch, convert2gray, convert2iterable
